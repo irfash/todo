e6a6dbefbb56892b32d0690c8e533d9f79f19798
irfash <irfashfarook@gmail.com>
2024-01-10 17:20:33 +0530

    boiler plate structure for project

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import Listheader from "./component/Listheader";
+import './index.css'
+function App() {
+  return <div className="app">
+    <Listheader listName = "Holiday tick list"/>
+  </div>
+}
+
+export default App;
